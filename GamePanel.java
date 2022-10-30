f64c4b53e129746cde7f6b9863d4ad694709a203
KApolinario1120 <kevinsquared@gmail.com>
2022-10-30 14:46:08 -0400

    Add variables for snake direction, apple placement and count, and a timer.

--- GamePanel.java
+++ GamePanel.java
@@ -11,6 +11,16 @@ public class GamePanel extends JPanel implements ActionListener {
     static final int UNIT_SIZE = 25;
     static final int GAME_UNITS = (SCREEN_WIDTH * SCREEN_HEIGHT) / UNIT_SIZE;
     static final int DELAY = 75;
+    final int x[] = new int[GAME_UNITS];
+    final int y[] = new int[GAME_UNITS];
+    int bodyParts = 6;
+    int applesEaten;
+    int appleX;
+    int appleY;
+    char direction = 'R'; //(R)ight, (L)eft, (U)p, (D)own
+    boolean running = false;
+    Timer timer;
+    Random random;
     
     GamePanel() {
 
